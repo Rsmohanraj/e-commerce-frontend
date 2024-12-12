3898ca8dba1aacff7f8e3282a71c056889b4aa57
Rsmohanraj <154896181+Rsmohanraj@users.noreply.github.com>
2024-12-12 14:00:55 +0530

    Update ProductSearch.js

--- src/compontents/ProductSearch.js
+++ src/compontents/ProductSearch.js
@@ -3,58 +3,35 @@ import MetaData from './MetaData';
 import Loader from '../compontents/Loader';
 import { getProducts } from '../Actions/productActions';
 import { useDispatch, useSelector } from 'react-redux';
-import Products from '../compontents/Products';
+import Product from '../compontents/Products';
 import { toast } from 'react-toastify';
 import Pagination from 'react-js-pagination'
 import { useParams } from 'react-router-dom';
-import Slider from 'rc-slider';
-import 'rc-slider/assets/index.css';
-import Tooltip from 'rc-tooltip';
-import 'rc-tooltip/assets/bootstrap.css'
 
- 
- 
-  export default function ProductSearch() {
+
+  export default function Home() {
     const dispatch = useDispatch();
     const {products,  loading , error,productsCount, resPerPage}=  useSelector((state) =>state.productsState)
     const [currentPage, setCurrentPage] = useState(1);
-    const [price, setPrice] = useState([1,1000]);
-    const [priceChanged, setPriceChanged] = useState(price);
-    const [category,setCategory] = useState(null);
-    const [rating,setRating] = useState(0);
 
-
-
-
-    const {keyword} = useParams();
-    const Categories =[
-      'Electronics',
-      'Accessories',
-      'Fashion',
-      'Headphones',
-      'Home',
-      'Sports',
-      'Toys',
-      'Laptops',
-      'Mobile Phones',
-      'Others'
-    ]
-
+   const {keyword} =useParams();
     const setCurrentPageNo = (pageNo) => {
 
-      setCurrentPage(pageNo);
+      setCurrentPage(pageNo)
     }
 
 
     
  useEffect(() =>{
   if(error){
- return  toast.success(error)
+ return  toast.error(error,{
+  position:"bottom-center"
+ })
   }
  
-   dispatch(getProducts(keyword, priceChanged,category,  rating,currentPage))
+   dispatch(getProducts(keyword))
 
- },[ error,dispatch,currentPage, keyword,priceChanged, category,rating]);
+ },[ error,dispatch,currentPage])
 
  
 
@@ -69,112 +46,12 @@ import 'rc-tooltip/assets/bootstrap.css'
 
   <section id="products" className="container mt-5">
      <div className="row">
-      <div className='col-6 col-md-3 mb-5 mt-5'>
-        {/* price Filter*/ }
-        <div className='px-5' onMouseUp={() =>setPriceChanged(price)}>
-       <Slider 
-       range={true}
-       marks={
-        {
-          1:"$1",
-          1000:"$1000"
-        }
-       }
-       min={1}
-       max={1000}
-       defaultValue={price}
-       onChange={(price)=>{
-        setPrice(price)
-       }}
-       handleRender={
-        renderProps => {
-          return (
-            <Tooltip  overlay={`$${renderProps.props['aria-valuenow']}`}>
-             <div {... renderProps.props}> </div>
-            </Tooltip>
-          );
-        }
-       }
-       
-       
-       />
-
-
-        </div>
-        <hr className='my-5' />
- {/* Category Filter*/ }
- <div className='mt-5'>
-<h3 className='mb-3'>Categories</h3>
-<ul className='pl-0'>
-  {Categories.map(category =>
-    <li
-   style={{
-    cursor:'pointer',
-    listStyleType:'none',
-   }}
-  key={category}
-  onClick={() =>{
-    setCategory(category)
-    
- 
-  }}
-  >
-    {category}
-
-  </li>
-
-  )}
-  
-</ul>
- </div>
- <hr className='my-5' />
-{/* Ratings Filter*/ }
-<div className='mt-5'>
-  <h4 className='mb-3'>Ratings</h4>
-  <ul className='pl-0'>
-  {[5,4,3,2,1].map(star =>
-    <li
-   style={{
-    cursor:'pointer',
-    listStyleType:'none',
-   }}
-  key={star}
-  onClick={() =>{
-    setRating(star)
-    
- 
-  }}
-  >
-   <div className='rating-outer'>
-    <div className='rating-inner'
-     style={{
-      width:`${star * 20}%`,
-      
-     }}
-    >
-
-    </div>
-   </div>
-
-  </li>
-
-  )}
-  
-</ul>
-
-</div>
-      </div>
-      <div className='col-6 col-md-9 '>
-        <div className='row'>
-        {products && products.map(product =>(
-      <Products col={4} key={product._id} product={product}/>
+    {products && products.map(product =>(
+      <Product  col={3}key={product._id} product={product}/>
 
 
     ))}
      
-        </div>
-      </div>
-    
    </div>
    </section>
    {productsCount > 0  && productsCount> resPerPage?
